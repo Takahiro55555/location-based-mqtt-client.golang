677482a844f36fa8b28a1a27da85c44521fba7a6
Takahiro55555 <tkhr.corei7@gmail.com>
2020-12-25 16:24:59 +0900

    update: インターフェースを１つに統合した

--- coordinates.go
+++ coordinates.go
@@ -1,10 +1,8 @@
 package client
 
-type CoordinateXYZ interface {
-	UpdateXYZ(x, y, z float64)
-	UpdateXY(x, y float64)
-	GetXYZ() (float64, float64, float64)
-	GetXY() (float64, float64)
+type Coordinate interface {
+	Update(a, b, c float64)
+	Get() (float64, float64, float64)
 }
 
 type coordinateXYZ struct {
@@ -13,38 +11,22 @@ type coordinateXYZ struct {
 	z float64
 }
 
-func NewCoordinateXYZ(x, y, z float64) CoordinateXYZ {
+func NewCoordinateXYZ(x, y, z float64) Coordinate {
 	return &coordinateXYZ{x, y, z}
 }
 
-func (c *coordinateXYZ) UpdateXYZ(x, y, z float64) {
+func (c *coordinateXYZ) Update(x, y, z float64) {
 	c.x = x
 	c.y = y
 	c.z = z
 }
 
-func (c *coordinateXYZ) UpdateXY(x, y float64) {
-	c.x = x
-	c.y = y
-}
-
-func (c *coordinateXYZ) GetXYZ() (float64, float64, float64) {
+func (c *coordinateXYZ) Get() (float64, float64, float64) {
 	return c.x, c.y, c.z
 }
 
-func (c *coordinateXYZ) GetXY() (float64, float64) {
-	return c.x, c.y
-}
-
-func NewCoordinateLLA(latitude, longitude, altitude float64) CoordinateLLA {
+func NewCoordinateLLA(latitude, longitude, altitude float64) Coordinate {
 	return &coordinateLLA{latitude: latitude, longitude: longitude, altitude: altitude}
-}
-
-type CoordinateLLA interface {
-	UpdateLLA(latitude, longitude, altitude float64)
-	UpdateLL(latitude, longitude float64)
-	GetLLA() (float64, float64, float64)
-	GetLL() (float64, float64)
 }
 
 type coordinateLLA struct {
@@ -53,18 +35,12 @@ type coordinateLLA struct {
 	altitude  float64 // 高度
 }
 
-func (c *coordinateLLA) UpdateLLA(latitude, longitude, altitude float64) {
+func (c *coordinateLLA) Update(latitude, longitude, altitude float64) {
 	c.latitude = latitude
 	c.longitude = longitude
 	c.altitude = altitude
 }
-func (c *coordinateLLA) UpdateLL(latitude, longitude float64) {
-	c.latitude = latitude
-	c.longitude = longitude
-}
-func (c *coordinateLLA) GetLLA() (float64, float64, float64) {
+
+func (c *coordinateLLA) Get() (float64, float64, float64) {
 	return c.latitude, c.longitude, c.altitude
-}
-func (c *coordinateLLA) GetLL() (float64, float64) {
-	return c.latitude, c.longitude
 }
--- coordinates_test.go
+++ coordinates_test.go
@@ -5,11 +5,11 @@ import (
 	"testing"
 )
 
-func TestGetLLA(t *testing.T) {
+func TestGet(t *testing.T) {
 	type args struct {
-		latitude  float64
-		longitude float64
-		altitude  float64
+		latitudeOrX  float64
+		longitudeOrY float64
+		altitudeOrZ  float64
 	}
 	tests := []struct {
 		name string
@@ -18,28 +18,34 @@ func TestGetLLA(t *testing.T) {
 		{
 			name: "Test 01",
 			args: args{
-				latitude:  10.,
-				longitude: 20.,
-				altitude:  30.,
+				latitudeOrX:  10.,
+				longitudeOrY: 20.,
+				altitudeOrZ:  30.,
 			},
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			c := client.NewCoordinateLLA(tt.args.latitude, tt.args.longitude, tt.args.altitude)
-			la, lo, al := c.GetLLA()
-			if la != tt.args.latitude || lo != tt.args.longitude || al != tt.args.altitude {
+			c := client.NewCoordinateXYZ(tt.args.latitudeOrX, tt.args.longitudeOrY, tt.args.altitudeOrZ)
+			x, y, z := c.Get()
+			if x != tt.args.latitudeOrX || y != tt.args.longitudeOrY || z != tt.args.altitudeOrZ {
+				t.Fatalf("Not match input and output.")
+			}
+
+			c = client.NewCoordinateLLA(tt.args.latitudeOrX, tt.args.longitudeOrY, tt.args.altitudeOrZ)
+			la, lo, al := c.Get()
+			if la != tt.args.latitudeOrX || lo != tt.args.longitudeOrY || al != tt.args.altitudeOrZ {
 				t.Fatalf("Not match input and output.")
 			}
 		})
 	}
 }
 
-func TestUpdateLLA(t *testing.T) {
+func TestUpdate(t *testing.T) {
 	type args struct {
-		latitude  float64
-		longitude float64
-		altitude  float64
+		latitudeOrX  float64
+		longitudeOrY float64
+		altitudeOrZ  float64
 	}
 	tests := []struct {
 		name string
@@ -48,197 +54,25 @@ func TestUpdateLLA(t *testing.T) {
 		{
 			name: "Test 01",
 			args: args{
-				latitude:  10.,
-				longitude: 20.,
-				altitude:  30.,
-			},
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			c := client.NewCoordinateLLA(0, 0, 0)
-			c.UpdateLLA(tt.args.latitude, tt.args.longitude, tt.args.altitude)
-			la, lo, al := c.GetLLA()
-			if la != tt.args.latitude || lo != tt.args.longitude || al != tt.args.altitude {
-				t.Fatalf("Not match input and output.")
-			}
-		})
-	}
-}
-
-func TestGetLL(t *testing.T) {
-	type args struct {
-		latitude  float64
-		longitude float64
-		altitude  float64
-	}
-	tests := []struct {
-		name string
-		args args
-	}{
-		{
-			name: "Test 01",
-			args: args{
-				latitude:  10.,
-				longitude: 20.,
-				altitude:  30.,
-			},
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			c := client.NewCoordinateLLA(tt.args.latitude, tt.args.longitude, tt.args.altitude)
-			la, lo := c.GetLL()
-			if la != tt.args.latitude || lo != tt.args.longitude {
-				t.Fatalf("Not match input and output.")
-			}
-		})
-	}
-}
-
-func TestUpdateLL(t *testing.T) {
-	type args struct {
-		latitude  float64
-		longitude float64
-	}
-	tests := []struct {
-		name string
-		args args
-	}{
-		{
-			name: "Test 01",
-			args: args{
-				latitude:  10.,
-				longitude: 20.,
-			},
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			c := client.NewCoordinateLLA(0, 0, 0)
-			c.UpdateLL(tt.args.latitude, tt.args.longitude)
-			la, lo := c.GetLL()
-			if la != tt.args.latitude || lo != tt.args.longitude {
-				t.Fatalf("Not match input and output.")
-			}
-		})
-	}
-}
-
-func TestGetXYZ(t *testing.T) {
-	type args struct {
-		x float64
-		y float64
-		z float64
-	}
-	tests := []struct {
-		name string
-		args args
-	}{
-		{
-			name: "Test 01",
-			args: args{
-				x: 10.,
-				y: 20.,
-				z: 30.,
-			},
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			c := client.NewCoordinateXYZ(tt.args.x, tt.args.y, tt.args.z)
-			x, y, z := c.GetXYZ()
-			if x != tt.args.x || y != tt.args.y || z != tt.args.z {
-				t.Fatalf("Not match input and output.")
-			}
-		})
-	}
-}
-
-func TestUpdateXYZ(t *testing.T) {
-	type args struct {
-		x float64
-		y float64
-		z float64
-	}
-	tests := []struct {
-		name string
-		args args
-	}{
-		{
-			name: "Test 01",
-			args: args{
-				x: 10.,
-				y: 20.,
-				z: 30.,
+				latitudeOrX:  10.,
+				longitudeOrY: 20.,
+				altitudeOrZ:  30.,
 			},
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			c := client.NewCoordinateXYZ(0, 0, 0)
-			c.UpdateXYZ(tt.args.x, tt.args.y, tt.args.z)
-			x, y, z := c.GetXYZ()
-			if x != tt.args.x || y != tt.args.y || z != tt.args.z {
+			c.Update(tt.args.latitudeOrX, tt.args.longitudeOrY, tt.args.altitudeOrZ)
+			x, y, z := c.Get()
+			if x != tt.args.latitudeOrX || y != tt.args.longitudeOrY || z != tt.args.altitudeOrZ {
 				t.Fatalf("Not match input and output.")
 			}
-		})
-	}
-}
 
-func TestGetXY(t *testing.T) {
-	type args struct {
-		x float64
-		y float64
-		z float64
-	}
-	tests := []struct {
-		name string
-		args args
-	}{
-		{
-			name: "Test 01",
-			args: args{
-				x: 10.,
-				y: 20.,
-				z: 30.,
-			},
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			c := client.NewCoordinateXYZ(tt.args.x, tt.args.y, tt.args.z)
-			x, y := c.GetXY()
-			if x != tt.args.x || y != tt.args.y {
-				t.Fatalf("Not match input and output.")
-			}
-		})
-	}
-}
-
-func TestUpdateXY(t *testing.T) {
-	type args struct {
-		x float64
-		y float64
-	}
-	tests := []struct {
-		name string
-		args args
-	}{
-		{
-			name: "Test 01",
-			args: args{
-				x: 10.,
-				y: 20.,
-			},
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			c := client.NewCoordinateXYZ(0, 0, 0)
-			c.UpdateXY(tt.args.x, tt.args.y)
-			x, y := c.GetXY()
-			if x != tt.args.x || y != tt.args.y {
+			c = client.NewCoordinateLLA(0, 0, 0)
+			c.Update(tt.args.latitudeOrX, tt.args.longitudeOrY, tt.args.altitudeOrZ)
+			la, lo, al := c.Get()
+			if la != tt.args.latitudeOrX || lo != tt.args.longitudeOrY || al != tt.args.altitudeOrZ {
 				t.Fatalf("Not match input and output.")
 			}
 		})
