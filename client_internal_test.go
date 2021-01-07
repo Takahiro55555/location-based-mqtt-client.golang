64c57579597c1182d346e1fcec95b07ff0e62659
Takahiro55555 <tkhr.corei7@gmail.com>
2021-01-07 09:52:06 +0900

    update: update: client 側仮完成（動作未確認） 複数トピックのSubscribeが可能になった

---
+++ client_internal_test.go
@@ -0,0 +1,67 @@
+package client
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestExtractUnduplicateTopics(t *testing.T) {
+	type args struct {
+		currentTopics []string
+		newTopics     []string
+	}
+	type want struct {
+		unsubscribeTopics []string
+		subscribeTopics   []string
+	}
+	tests := []struct {
+		name string
+		args args
+		want want
+	}{
+		{
+			name: "Test 01",
+			args: args{
+				currentTopics: []string{"/0/#"},
+				newTopics:     []string{"/1/#"},
+			},
+			want: want{
+				unsubscribeTopics: []string{"/0/#"},
+				subscribeTopics:   []string{"/1/#"},
+			},
+		},
+		{
+			name: "Test 02",
+			args: args{
+				currentTopics: []string{"/0/#", "/1/#"},
+				newTopics:     []string{"/1/#", "/2/#"},
+			},
+			want: want{
+				unsubscribeTopics: []string{"/0/#", ""},
+				subscribeTopics:   []string{"", "/2/#"},
+			},
+		},
+		{
+			name: "Test 03",
+			args: args{
+				currentTopics: []string{"/1/#"},
+				newTopics:     []string{"/1/#"},
+			},
+			want: want{
+				unsubscribeTopics: []string{""},
+				subscribeTopics:   []string{""},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ut, st := extractUnduplicateTopics(tt.args.currentTopics, tt.args.newTopics)
+			if !reflect.DeepEqual(ut, tt.want.unsubscribeTopics) {
+				t.Errorf("UnsubscribeTopics Want: %v, Result: %v", tt.want.unsubscribeTopics, ut)
+			}
+			if !reflect.DeepEqual(st, tt.want.subscribeTopics) {
+				t.Errorf("SubscribeTopics Want: %v, Result: %v", tt.want.subscribeTopics, st)
+			}
+		})
+	}
+}
