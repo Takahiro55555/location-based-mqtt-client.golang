a988c679a9b77593849e94bbef01def77fe9e23b
Takahiro55555 <tkhr.corei7@gmail.com>
2021-01-18 18:47:12 +0900

    update: テストコードを移動

--- client_internal_test.go
+++ client_internal_test.go
@@ -3,8 +3,6 @@ package client
 import (
 	"reflect"
 	"testing"
-
-	"github.com/golang/geo/s2"
 )
 
 func TestExtractUnduplicateTopics(t *testing.T) {
@@ -54,6 +52,28 @@ func TestExtractUnduplicateTopics(t *testing.T) {
 				subscribeTopics:   []string{""},
 			},
 		},
+		{
+			name: "Test 04",
+			args: args{
+				currentTopics: []string{"/1/2/#", "/1/3/#"},
+				newTopics:     []string{"/1/#"},
+			},
+			want: want{
+				unsubscribeTopics: []string{"/1/2/#", "/1/3/#"},
+				subscribeTopics:   []string{"/1/#"},
+			},
+		},
+		{
+			name: "Test 05",
+			args: args{
+				currentTopics: []string{"/1/2/#", "/1/3/#"},
+				newTopics:     []string{"/1/2/#", "/1/3/4/#"},
+			},
+			want: want{
+				unsubscribeTopics: []string{"", "/1/3/#"},
+				subscribeTopics:   []string{"", "/1/3/4/#"},
+			},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -63,40 +83,6 @@ func TestExtractUnduplicateTopics(t *testing.T) {
 			}
 			if !reflect.DeepEqual(st, tt.want.subscribeTopics) {
 				t.Errorf("SubscribeTopics Want: %v, Result: %v", tt.want.subscribeTopics, st)
-			}
-		})
-	}
-}
-
-func TestCelID2TopicName(t *testing.T) {
-	type args struct {
-		lat float64
-		lng float64
-	}
-	type want struct {
-		topic string
-	}
-	tests := []struct {
-		name string
-		args args
-		want want
-	}{
-		{
-			name: "Test 01",
-			args: args{
-				lat: 1,
-				lng: 1,
-			},
-			want: want{
-				topic: "/0/2/0/0/0/0/0/2/2/0/2/0/0/2/2/2/2/0/0/1/0/0/0/0/2/1/1/3/2/2/2",
-			},
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			topic := celID2TopicName(s2.CellIDFromLatLng(s2.LatLngFromDegrees(tt.args.lat, tt.args.lng)))
-			if topic != tt.want.topic {
-				t.Errorf("UnsubscribeTopics Want: %v, Result: %v", tt.want.topic, topic)
 			}
 		})
 	}
